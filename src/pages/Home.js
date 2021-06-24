bd92108e615c39b0a672df30887216b0e7879530
rakib <rakibulislam202019@gmail.com>
2021-06-24 13:37:02 +0600

    mobile responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,13 @@ import img1 from "../images/study 1 (1).jpg";
 const Home = () => {
   return (
     <>
+      {/* Navbar section */}
       <div className="navBar">
         <h1>SONGBIRD</h1>
         <button>Contact Us</button>
       </div>
+
+      {/* Hero section */}
       <div className="hero">
         <div>
           <h1>STUDY, WORKOUT, AND CHILL TOGETHER</h1>
@@ -17,7 +20,7 @@ const Home = () => {
           </p>
         </div>
 
-        <div>
+        <div className="buttonDiv">
           <button>
             <i className="fab fa-apple"></i> App Store
           </button>
@@ -27,6 +30,7 @@ const Home = () => {
         </div>
       </div>
 
+      {/* Main section */}
       <div className="main">
         <img
           className="image"
@@ -37,6 +41,7 @@ const Home = () => {
         />
       </div>
 
+      {/* Footer section */}
       <div className="footer">
         <div className="footerPart1">
           <p>Terms</p>
